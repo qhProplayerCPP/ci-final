8f25b72f0c0f751abe5da7c8a7eb1cd966551a97
jason1 <quang.thn0837@sinhvien.hoasen.edu.vn>
2021-03-02 03:11:48 +0700

    done css signup and login

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -6,14 +6,34 @@ import { SignIn } from '../Utilities';
 export class Login extends React.Component {
   render() {
     return (
-      <div className="LoginBox">
-        <h1>Login</h1>
-        <label>Username:</label>
-        <input type="text" id="LoginUsername" />
-        <label>Password:</label>
-        <input type="text" id="LoginPassword" />
-        <Link to="/sign-up">Sign up</Link>
-        <button
+      <div className="login__Box">
+        <div className="spotiLogin">
+          <img
+            alt="SpotiIcon"
+            className="spotiIcon"
+            src="https://image.flaticon.com/icons/png/512/2111/2111624.png"
+          />
+          <h1>Welcome to Spotifry</h1>
+        </div>
+        <h2 className="login">Login</h2>
+
+        <div className="textbox">
+          <i className="iconLogin fas fa-user"></i>
+          <input type="text" placeholder="Username" id="LoginUsername" />
+        </div>
+
+        <div className="textbox">
+          <i className="iconLogin fas fa-key"></i>
+          <input type="password" placeholder="Password" id="LoginPassword" />
+        </div>
+        <span className="suggestSU">
+          Don't have an account ?
+          <Link className="signUpLink" to="/sign-up">
+            Sign up
+          </Link>
+        </span>
+        <a
+          className="loginButton"
           onClick={() => {
             const data = {
               username: document.getElementById('LoginUsername').value,
@@ -26,8 +46,13 @@ export class Login extends React.Component {
             });
           }}
         >
-          Sign in
-        </button>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Sign In
+        </a>
+        
       </div>
     );
   }
--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,18 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../Css/SignUp.css';
+import '../Css/Login.css';
 import { SignUpF } from '../Utilities';
 export class SignUp extends React.Component {
   render() {
     return (
-      <div className="LoginBox">
-        <h1>Sign up</h1>
-        <label>Username:</label>
-        <input type="text" id="SignUpUsername" />
-        <label>Password:</label>
-        <input type="text" id="SignUpPassword" />
-        <Link to="/">Login</Link>
-        <button
+      <div className="login__Box">
+        <div className="spotiLogin">
+          <img
+            alt="SpotiIcon"
+            className="spotiIcon"
+            src="https://image.flaticon.com/icons/png/512/2111/2111624.png"
+          />
+          <h1>Welcome to Spotifry</h1>
+        </div>
+        <h2 className="login">Sign up</h2>
+        <div className="textbox">
+          <i className="iconLogin fas fa-user"></i>
+          <input type="text" placeholder="Username" id="SignUpUsername" />
+        </div>
+
+        <div className="textbox">
+          <i className="iconLogin fas fa-key"></i>
+          <input type="password" placeholder="Password" id="SignUpPassword" />
+        </div>
+
+        <span className="suggestSU">
+          Already have an account ?
+          <Link className="signUpLink" to="/">
+            Login
+          </Link>
+        </span>
+        
+        <a
+          className="loginButton"
           onClick={() => {
             const data = {
               username: document.getElementById('SignUpUsername').value,
@@ -29,8 +50,12 @@ export class SignUp extends React.Component {
             }
           }}
         >
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
           Sign up
-        </button>
+        </a>
       </div>
     );
   }
